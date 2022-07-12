ec5d46eccafba33da01f61dd9d4a9428746e0586
namnv2496 <namnv2496@gmail.com>
2022-07-12 23:35:10 +0700

    new website

---
+++ src/components/MenuItems.js
@@ -0,0 +1,38 @@
+export const MenuItems = [
+  {
+    title: 'item',
+    path: '/item',
+    cName: 'dropdown-link'
+  }
+];
+
+export const MenuJavaItems = [
+  {
+    title: 'OSGI - Idempiere',
+    path: '/OSGI',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Java Core',
+    path: '/javaCore',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Spring boot',
+    path: '/springBoot',
+    cName: 'dropdown-link'
+  }
+];
+
+export const MenuLinuxItems = [
+  {
+    title: 'C/C++',
+    path: '/c-c++',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Linux Driver',
+    path: '/linuxDriver',
+    cName: 'dropdown-link'
+  }
+];
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Dropdown from './Dropdown';
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
+  const [dropdown, setDropdown] = useState(false);
+  const [dropdown1, setDropdown1] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -15,6 +18,38 @@ function Navbar() {
       setButton(false);
     } else {
       setButton(true);
+    }
+  };
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+    const onMouseEnter1 = () => {
+    if (window.innerWidth < 960) {
+      setDropdown1(false);
+    } else {
+      setDropdown1(true);
+    }
+  };
+
+  const onMouseLeave1 = () => {
+    if (window.innerWidth < 960) {
+      setDropdown1(false);
+    } else {
+      setDropdown1(false);
     }
   };
 
@@ -29,26 +64,33 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            TRVL
+            LIAM
             <i class='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                Home
-              </Link>
+            <li className='nav-item'
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <p className='nav-links'>
+                Linux <i className='fas fa-caret-down' />
+              </p>
+              {dropdown && <Dropdown menuType='1' />}
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/services'
+            <li className='nav-item'
+                onMouseEnter={onMouseEnter1}
+                onMouseLeave={onMouseLeave1}
+            >
+              <p
                 className='nav-links'
-                onClick={closeMobileMenu}
               >
-                Services
-              </Link>
+                Java <i className='fas fa-caret-down' />
+              </p>
+              {dropdown1 && <Dropdown menuType='0' />}
+              
             </li>
             <li className='nav-item'>
               <Link
