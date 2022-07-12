33108f40d065337ee0ae8ac774e7ccfc8440f324
namnv2496 <namnv2496@gmail.com>
2022-07-12 23:45:06 +0700

    Updates

---
+++ precache-manifest.9a8c62a552163218e2a6d15ff6af89bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d621c9cf3d3f54dcd6c85b7003f03b37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3f8895db3d3f8e680c",
+    "url": "/static/css/main.e830d1d9.chunk.css"
+  },
+  {
+    "revision": "57aa0c479d535009e5b8",
+    "url": "/static/js/2.cae2b360.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cae2b360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3f8895db3d3f8e680c",
+    "url": "/static/js/main.a7526b5f.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
