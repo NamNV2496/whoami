61554cf8d48e1696b6fd3ac83d6694e86e0286cc
namnv2496 <namnv2496@gmail.com>
2022-07-13 20:41:20 +0700

    Updates

---
+++ precache-manifest.4a664cb6e6e45f77830e7d9bdc8dba8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f21d836745d10bf1c109156cc0d223e",
+    "url": "/whoami/index.html"
+  },
+  {
+    "revision": "ff2c11fc5b56def2a215",
+    "url": "/whoami/static/css/main.4919279e.chunk.css"
+  },
+  {
+    "revision": "e74344f30cf529a4770f",
+    "url": "/whoami/static/js/2.386093fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/whoami/static/js/2.386093fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff2c11fc5b56def2a215",
+    "url": "/whoami/static/js/main.d78d0d9b.chunk.js"
+  },
+  {
+    "revision": "acb5ded898d21d37dae2",
+    "url": "/whoami/static/js/runtime-main.8acfe459.js"
+  }
+]);
